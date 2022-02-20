8c6c8ba51d85f363be8006b73b1b02c8714e552f
u1f98e <u1f98e@gmail.com>
2022-02-19 22:38:05 -0800

    Change backend framework (for the last time)

--- src/server/main.rs
+++ src/server/main.rs
@@ -3,33 +3,27 @@ mod class;
 mod post;
 mod sql;
 
+use std::path::PathBuf;
 use std::{path::Path, sync::Arc};
 
 use async_std;
 use sqlx::mysql::MySqlPoolOptions as PoolOptions;
-use web_server::{self, Request};
 
+#[macro_use] extern crate rocket;
+use rocket::fs::NamedFile;
 
-#[async_std::main]
-async fn main() {
-    // Create the connection pool for the sql server
-    // let pool = PoolOptions::new()
-    //     .max_connections(5)
-    //     .connect("").await.unwrap();
-    //
-    // *sql::sql_pool.lock().unwrap() = Some(pool);
-
-    let server = web_server::new()
-        .get("/", Box::new(|request, mut default_response|
-            Path::new("src/client/index.html").into()))
-        .get("/:page", Box::new(|request, mut default_response| {
-            println!("Request: {}", request.params.get("page").unwrap());
-            let page = request.params.get("page").unwrap();
-            Path::new(&format!("src/client/{page}")).into()
-        }))
-        .post("/create-student/:json", Box::new(move |request: Request, response| 
-            student::from_json(request.params.get("json").unwrap()).into() ));
-        
-    server.launch(8080); // Run the server
+#[get("/")]
+async fn index() -> Option<NamedFile> {
+    NamedFile::open(Path::new("src/client/index.html")).await.ok()
 }
 
+#[get("/<file..>")]
+async fn files(file: PathBuf) -> Option<NamedFile> {
+    println!("{:?}", file);
+    NamedFile::open(Path::new("src/client/").join(file)).await.ok()
+}
+
+#[launch]
+fn launch() -> _ {
+    rocket::build().mount("/", routes![index, files])
+}
